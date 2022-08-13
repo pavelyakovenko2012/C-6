bbd8ce57f48e94836c10c7a3bbbf1dbf48d358f4
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-08-13 14:55:49 +0500

    002

---
+++ C002/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+Console.Write("Введите количество строк массива: ");
+int rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns,0,10);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(ChangeArray(array));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+int[,] result = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+result[i, j] = new Random().Next(min, max+1);
+}
+
+}
+return result;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+if (i % 2 == 0 && j % 2 == 0)
+{
+array[i, j] *= array[i, j]; // 5*5 = 25
+}
+}
+}
+return array;
+}
+
+void PrintArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write($"{array[i, j]} ");
+}
+Console.WriteLine();
+}
+}
